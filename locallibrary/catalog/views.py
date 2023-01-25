a89808c74247b93373b876b4525d8e0289d3784d
potemkina-92@list.ru <potemkina-92@list.ru>
2023-01-25 14:52:44 +0300

    modules 1-6

---
+++ locallibrary/catalog/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.views import generic
+
+from .models import Book, Author, BookInstance, Genre
+
+
+def index(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    # Генерация "количеств" некоторых главных объектов
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Доступные книги (статус = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()  # Метод 'all()' применён по умолчанию.
+    num_genre = Genre.objects.count()
+
+    # Отрисовка HTML-шаблона index.html с данными внутри
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors, 'num_genre': num_genre}
+    )
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+    #
+    # def get_queryset(self):
+    #     return Book.objects.filter(title__icontains='war')[:5]  # Получить 5 книг, содержащих 'war' в заголовке
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
